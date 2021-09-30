edd05e4aee01f1fd3fa09617a3d91bed54206dfa
andrescvargasr <andresc.vargasr@gmail.com>
2021-09-30 10:44:25 +0200

    fix: status.h

---
+++ BioMain/lib/hack/status.cpp
@@ -0,0 +1,43 @@
+#include "hackConstants.h"
+#include <A_Parameters.h>
+
+bool isError( int filter) {
+  if ((getParameter(PARAM_ERROR) & filter) != 0) return true;
+  return false;
+}
+
+bool isError() {
+  return (getParameter(PARAM_ERROR) != 0);
+}
+
+bool getStatus(byte statusFlag) {
+  return getParameterBit(PARAM_STATUS, statusFlag);
+}
+
+bool isEnabled(byte statusFlag) {
+  return getParameterBit(PARAM_ENABLED, statusFlag);
+}
+
+bool isRunning(byte statusFlag) {
+  return getParameterBit(PARAM_STATUS, statusFlag);
+}
+
+bool startProcess(byte statusFlag) {
+  boolean statusChanged = setParameterBit(PARAM_STATUS, statusFlag);
+#ifdef EVENT_LOGGING
+  if (statusChanged) {
+    writeLog(EVENT_STATUS_ENABLE, statusFlag);
+  }
+#endif
+  return statusChanged;
+}
+
+bool stopProcess(byte statusFlag) {
+  bool statusChanged =  clearParameterBit(PARAM_STATUS, statusFlag);
+#ifdef EVENT_LOGGING
+  if (statusChanged) {
+    writeLog(EVENT_STATUS_DISABLE, statusFlag);
+  }
+#endif
+  return statusChanged;
+}
--- BioMain/lib/hack/status.h
+++ BioMain/lib/hack/status.h
@@ -1,42 +1,9 @@
+#include <Arduino.h>
 
-boolean isError(int filter) {
-  if ((getParameter(PARAM_ERROR) & filter) != 0) return true;
-  return false;
-}
-
-boolean isError() {
-  return (getParameter(PARAM_ERROR) != 0);
-}
-
-boolean getStatus(byte statusFlag) {
-  return getParameterBit(PARAM_STATUS, statusFlag);
-}
-
-boolean isEnabled(byte statusFlag) {
-  return getParameterBit(PARAM_ENABLED, statusFlag);
-}
-
-boolean isRunning(byte statusFlag) {
-  return getParameterBit(PARAM_STATUS, statusFlag);
-}
-
-
-boolean startProcess(byte statusFlag) {
-  boolean statusChanged = setParameterBit(PARAM_STATUS, statusFlag);
-#ifdef EVENT_LOGGING
-  if (statusChanged) {
-    writeLog(EVENT_STATUS_ENABLE, statusFlag);
-  }
-#endif
-  return statusChanged;
-}
-
-boolean stopProcess(byte statusFlag) {
-  boolean statusChanged =  clearParameterBit(PARAM_STATUS, statusFlag);
-#ifdef EVENT_LOGGING
-  if (statusChanged) {
-    writeLog(EVENT_STATUS_DISABLE, statusFlag);
-  }
-#endif
-  return statusChanged;
-}
+bool isError( int );
+bool isError();
+bool getStatus( byte );
+bool isEnabled( byte );
+bool isRunning( byte );
+bool startProcess( byte );
+bool stopProcess( byte );
