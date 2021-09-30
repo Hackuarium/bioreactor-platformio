6203c004dbec29380fca619d071b1428ad4f996a
andrescvargasr <andresc.vargasr@gmail.com>
2021-09-30 16:14:11 +0200

    fix: BioMain.cpp

--- BioMain/src/BioMain.cpp
+++ BioMain/src/BioMain.cpp
@@ -1,15 +1,38 @@
 #define SOFTWARE_VERSION "v0.0.1"
 
-#include <Arduino.h>
+#define THR_WIRE_MASTER 1
+#define THR_SST_LOGGER  1
+#define THR_SERIAL      1
+#define THR_ONEWIRE     1
+#define THR_PID         1
+#define THR_FAN         1
+#define THR_ERROR       1
+#define THR_STEPPER     1
+#define THR_STEPS       1
+#define THR_WEIGHT      1
+#define THR_OUTPUTS     1
+
 #include "A_Parameters.h"
 #include "BioMain.h"
+#include <ChNil.h>
 
+/*********
+   SETUP
+ *********/
 void setup() {
   delay(1000);
   Serial.begin(9600);
   delay(1000);
+  setupParameters();
+
+#ifdef FLASH_SELECT
+  pinMode(FLASH_SELECT, OUTPUT);
+  setupMemory();
+  recoverLastEntryN();
+  loadLastEntryToParameters();   //get back the previous config
+#endif
+
+  chBegin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() {}
