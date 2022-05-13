567098921d47bc5b03ff06550a28be668115437f
andrescvargasr <andresc.vargasr@gmail.com>
2022-05-12 19:04:45 -0500

    fix: equation to wait between every log

--- BioMain/src/SSTThread.cpp
+++ BioMain/src/SSTThread.cpp
@@ -12,12 +12,15 @@
 THD_FUNCTION(ThreadLogger, arg) {
   chThdSleep(5000);
   writeLog(EVENT_ARDUINO_BOOT, 0);
+  chThdSleepSeconds((long)LOG_INTERVAL - (long)(millis() / 1000UL) + 5UL);
   while (TRUE) {
     // avoids logging during the second x+1, ensure x+LOG_INTERVAL
     // because epoch is only precise to the second so the logging is evenly
     // spaced
-    chThdSleep(LOG_INTERVAL * 1000 - millis() % 1000 + 100);
+    // unsigned long waiting = (unsigned long)LOG_INTERVAL - (millis() % 1000UL);
+    // chThdSleep((long)LOG_INTERVAL * 1000UL - millis() % 1000UL + 100UL);
     writeLog(0, 0);
+    chThdSleepSeconds(LOG_INTERVAL);
   }
 }
 
